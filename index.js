e1efe6ce9230338820f1b1d0285aabe3a95d0b42
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-06-28 00:36:28 +0300

    completed asssignments

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(){
+    let a=2000
+    let b=100
+    let result=a/b
+    return result
+}
+
+console.log(divide(2000,100))
+const square=(a)=>{
+    return a*a;
+}
+
+const add=(a,b)=>{
+    return a+b;
+}
